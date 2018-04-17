d37f376f62c7ac53ceb5252d3649e418adce0cf6
Anike Arni <aarni@thoughtworks.com>
2018-04-17 19:09:57 -0400

    persists image and story in redis

--- index.py
+++ index.py
@@ -1,19 +1,23 @@
 import eel
 import string
 import random
+import redis
 #  from neural import generate
 
 eel.init('web')
 #  z = generate.load_all()
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 @eel.expose
 def generate_story(image):
-    filename = 'images/' + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20)) + '.jpg'
-    fh = open(filename, "wb")
+    filename = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
+    filepath = 'images/' + filename + '.jpg'
+    fh = open(filepath, "wb")
     fh.write(image.decode('base64'))
     fh.close()
+    story = 'story' # generate.story(z, filename)
+    r.hset(filename, 'image', filepath, 'story', story)
     return 'story'
-    #  return generate.story(z, filename)
 
 _options = { 'mode': 'None' }
 eel.start('bundle/index.html', options=_options)
