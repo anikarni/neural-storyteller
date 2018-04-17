4fb08fe380b21d37127950347f950ad183cd240c
Bruna Pereira <bpereira@thoughtworks.com>
2018-04-17 13:32:23 -0400

    adds webcamjs

--- web/index.js
+++ web/index.js
@@ -1,1 +1,11 @@
-console.log('does it work?')
+const Webcam = require('webcamjs')
+
+Webcam.attach('#my_camera')
+
+const takeSnapshot = () => {
+  Webcam.snap((data_uri) => {
+    document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/>';
+  });
+}
+
+global.takeSnapshot = takeSnapshot
