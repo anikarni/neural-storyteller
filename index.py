8ff5383b5b0c8ae3ff5032c58483c6237736bd2d
Bruna Pereira <pprbruna2@gmail.com>
2018-04-17 18:28:54 -0400

    wip: create jpg from base64

--- index.py
+++ index.py
@@ -5,8 +5,15 @@ eel.init('web')
 z = generate.load_all()
 
 @eel.expose
+def create_image(image):
+    fh = open("imageToSave.jpg", "wb")
+    fh.write(img_data.decode('base64'))
+    fh.close()
+
+@eel.expose
 def generate_story(image):
     return generate.story(z, image)
 
 _options = { 'mode': 'None' }
 eel.start('bundle/index.html', options=_options)
+
