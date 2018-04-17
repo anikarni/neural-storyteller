3346be79b16e7d05c690d441758397dcd504b8f8
Anike Arni <aarni@thoughtworks.com>
2018-04-17 19:16:17 -0400

    fix saving to redis

--- index.py
+++ index.py
@@ -15,9 +15,10 @@ def generate_story(image):
     fh = open(filepath, "wb")
     fh.write(image.decode('base64'))
     fh.close()
-    story = 'story' # generate.story(z, filename)
-    r.hset(filename, 'image', filepath, 'story', story)
-    return 'story'
+    story = 'We had to the group of people at the beach , but for the first time in my life , it felt like they were going to fall apart . I was n\'t really sure what the beach was going to be . I had never seen Nate so beautiful , and he had no intention of helping her out on the beach . As soon as the sun came up , she shook her head and let go of my arm . Heat spread through my body , reminding me that Lissa and I have been friends for almost twenty-four thousand years . I was more than willing to ignore the group , at the beach people left .' # generate.story(z, filename)
+    r.hset(filename, 'image', filepath)
+    r.hset(filename, 'story', story)
+    return story
 
 _options = { 'mode': 'None' }
 eel.start('bundle/index.html', options=_options)
