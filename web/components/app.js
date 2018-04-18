4194688fdcbf1f6846b5184a503697224853806f
Bruna Pereira <pprbruna2@gmail.com>
2018-04-18 15:06:45 -0400

    creates Camera component

--- web/components/app.js
+++ web/components/app.js
@@ -1,14 +1,14 @@
 import React from 'react'
+import Hero from './hero'
+import Camera from './Camera'
 
 const App = () =>
   <div>
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="index.html">Header</a>
     </nav>
-    <div className="jumbotron">
-      <h1 className="display-4">Hello, world!</h1>
-      <a href="take-picture.html" className="btn btn-primary active">Take Pic</a>
-    </div>
+    <Hero />
+    <Camera />
     <footer className="footer">
       <span className="text-muted">Footer</span>
     </footer>
---
+++ web/components/hero.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () =>
+  <div className="jumbotron">
+    <h1 className="display-4">Hello, world!</h1>
+    <a href="take-picture.html" className="btn btn-primary active">Take Pic</a>
+  </div>
+
+export default Hero
