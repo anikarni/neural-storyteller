63126516790c073d51ec6d8638738d21deb31ff7
Bruna Pereira <pprbruna2@gmail.com>
2018-04-18 15:11:55 -0400

    adds webcamjs setup

--- web/components/camera.js
+++ web/components/camera.js
@@ -1,25 +1,33 @@
 import React from 'react'
-
-//Webcam.set({
-    //width: 700,
-    //height: 530,
-    //dest_width: 640,
-    //dest_height: 480,
-    //image_format: 'jpeg',
-    //jpeg_quality: 90,
-    //force_flash: false,
-    //flip_horiz: true,
-    //fps: 45
-//})
-
-//Webcam.attach('#my_camera')
+import Webcam from 'webcamjs'
 
 const takeSnapshot = () => console.log('snapshot')
 
-const Camera = () =>
-  <div id="take-picture" className="container">
-    <div id="my_camera"></div>
-    <a onClick={takeSnapshot}>Take Snapshot</a>
-  </div>
+class Camera extends React.Component {
+  componentDidMount() {
+    Webcam.set({
+        width: 700,
+        height: 530,
+        dest_width: 640,
+        dest_height: 480,
+        image_format: 'jpeg',
+        jpeg_quality: 90,
+        force_flash: false,
+        flip_horiz: true,
+        fps: 45
+    })
+
+    Webcam.attach('#my_camera')
+  }
+
+  render() {
+    return (
+      <div id="take-picture" className="container">
+        <div id="my_camera"></div>
+        <a onClick={takeSnapshot}>Take Snapshot</a>
+      </div>
+    )
+  }
+}
 
 export default Camera
